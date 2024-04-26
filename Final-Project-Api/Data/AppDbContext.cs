9fd6892aaad7013a878b21cf58f73861ed9c1f14
Mohamed Ali <mohammed.ali@tera.ltd>
2024-04-26 23:53:28 +0300

    Modify Doctor and Patient identity relation

--- Final-Project-Api/Data/AppDbContext.cs
+++ Final-Project-Api/Data/AppDbContext.cs
@@ -5,13 +5,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Final_Project_Api.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Appointment> Appointments { get; set; }
         public DbSet<Medicine> Medicines { get; set; }
         public DbSet<AppointmentMedicine> AppointmentMedicines { get; set; }
-        public virtual DbSet<Patient> Patients { get; set; }
-        public virtual DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
         public DbSet<AnalysisType> AnalysisTypes { get; set; }
         public DbSet<AppointmentAnalysis> AppointmentAnalyses { get; set; }
         public DbSet<AppointmentDiagnose> AppointmentDiagnoses { get; set; }
